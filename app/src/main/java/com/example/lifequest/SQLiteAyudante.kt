40510bf0e642bad1d62393cabeee122fda4b3c71
Netzahre <netzahre@gmail.com>
2024-11-29 09:58:38 +0000

    Wip 2

--- app/src/main/java/com/example/lifequest/SQLiteAyudante.kt
+++ app/src/main/java/com/example/lifequest/SQLiteAyudante.kt
@@ -6,13 +6,13 @@ import android.database.sqlite.SQLiteOpenHelper
 
 class SQLiteAyudante(contexto: Context, name: String, factory: SQLiteDatabase.CursorFactory, version: Int) : SQLiteOpenHelper(contexto, name, factory, version) {
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL("CREATE TABLE usuarios (nombre TEXT PRIMARY KEY, correo TEXT, contrasena TEXT)")
-        db?.execSQL("CREATE TABLE IF NOT EXISTS preferencias (usuario TEXT PRIMARY KEY, is_night_mode INTEGER)")
-        db?.execSQL("CREATE TABLE comidas (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, calorias INTEGER, proteina INTEGER, carbohidratos INTEGER, grasas INTEGER)")
-        db?.execSQL("CREATE TABLE ejercicios (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, calorias INTEGER, tiempo INTEGER)")
-        db?.execSQL("CREATE TABLE dias (id INTEGER PRIMARY KEY AUTOINCREMENT, fecha TEXT, calorias INTEGER, proteina INTEGER, carbohidratos INTEGER, grasas INTEGER)")
-        db?.execSQL("CREATE TABLE comidas_dia (id INTEGER PRIMARY KEY AUTOINCREMENT, id_comida INTEGER, id_dia INTEGER)")
-        db?.execSQL("CREATE TABLE ejercicios_dia (id INTEGER PRIMARY KEY AUTOINCREMENT, id_ejercicio INTEGER, id_dia INTEGER)")
+        db?.execSQL("CREATE TABLE usuarios (nombre TEXT PRIMARY KEY, correo TEXT, contrasena TEXT, monedas INTEGER, tareas_completadas INTEGER, monedas_gastadas INTEGER)")
+        db?.execSQL("CREATE TABLE Upreferencias (usuario TEXT PRIMARY KEY, is_night_mode INTEGER)")
+        db?.execSQL("CREATE TABLE misiones (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, descripcion TEXT, fecha TEXT, usuario TEXT, vecesCompletada INTEGER)")
+        db?.execSQL("CREATE TABLE recompensas (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, descripcion TEXT, costo INTEGER, usuario TEXT)")
+        db?.execSQL("CREATE TABLE misiones_recompensas (id INTEGER PRIMARY KEY AUTOINCREMENT, id_mision INTEGER, id_recompensa INTEGER)")
+
+
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
